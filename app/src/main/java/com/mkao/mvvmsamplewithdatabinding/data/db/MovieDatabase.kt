fc38fa4f9445901fb251724459755fef7b08eff3
mkao <mkaoibrahim@gmail.com>
2022-05-19 13:52:35 -1200

    DB Updates

--- app/src/main/java/com/mkao/mvvmsamplewithdatabinding/data/db/MovieDatabase.kt
+++ app/src/main/java/com/mkao/mvvmsamplewithdatabinding/data/db/MovieDatabase.kt
@@ -1,4 +1,30 @@
 package com.mkao.mvvmsamplewithdatabinding.data.db
 
-class MovieDatabase {
+import android.app.Application
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.mkao.mvvmsamplewithdatabinding.data.model.Movie
+
+@Database(entities = [Movie::class],version=1)
+@TypeConverters(genreIDconverter::class)
+abstract class MovieDatabase: RoomDatabase() {
+abstract fun MovieDao():MovieDao
+
+companion object{
+    private val lock = Any()
+    private const val DB_NAME = "MovieDatabase"
+    private var INSTANCE:MovieDatabase?=null
+
+    fun getInstance(application:Application):MovieDatabase{
+        synchronized(MovieDatabase.lock){
+            if (MovieDatabase.INSTANCE==null){
+                MovieDatabase.INSTANCE =Room.databaseBuilder(application,MovieDatabase::class.java,MovieDatabase.DB_NAME)
+                    .build()
+            }
+        }
+        return INSTANCE!!
+    }
+  }
 }
