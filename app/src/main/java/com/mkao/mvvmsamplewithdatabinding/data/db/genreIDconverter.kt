f55e71295e49d68e7b0e3b40443785bcf81b6176
mkao <mkaoibrahim@gmail.com>
2022-05-20 05:08:55 +0300

    DB Updates

--- app/src/main/java/com/mkao/mvvmsamplewithdatabinding/data/db/genreIDconverter.kt
+++ app/src/main/java/com/mkao/mvvmsamplewithdatabinding/data/db/genreIDconverter.kt
@@ -1,4 +1,26 @@
 package com.mkao.mvvmsamplewithdatabinding.data.db
 
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
 class genreIDconverter {
+    val gson =Gson()
+
+    @TypeConverter
+    fun stringToGenreList(data:String?):List<Int>{
+        if (data==null){
+            return Collection.emptyList()
+        }
+        val listype = object : TypeToken<List<Int>>() {}.type
+        return gson.fromJson(data,listype)
+    }
+    @TypeConverter
+    fun genreListToString(genreIds:List<Int>?):String{
+        if (genreIds==null){
+            return gson.toJson(Collection.emptyList<Int>())
+        }
+        return gson.toJson(genreIds)
+    }
 }
