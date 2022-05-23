67346808058721550f8f90f7ef462ec81a6ae3ab
mkao <mkaoibrahim@gmail.com>
2022-05-23 22:06:51 +0300

    Retrofit Requests Updated

--- app/src/main/java/com/mkao/mvvmsamplewithdatabinding/data/MovieRepository.kt
+++ app/src/main/java/com/mkao/mvvmsamplewithdatabinding/data/MovieRepository.kt
@@ -1,4 +1,15 @@
 package com.mkao.mvvmsamplewithdatabinding.data
 
-class MovieRepository {
+import androidx.lifecycle.LiveData
+import com.mkao.mvvmsamplewithdatabinding.data.model.Movie
+import retrofit2.http.Query
+
+interface MovieRepository {
+    fun getSavedMovies(): LiveData<List<Movie>>
+
+    fun saveMovie(movie: Movie)
+
+    fun deleteMovie(movie: Movie)
+
+    fun searchMovies(query: String):LiveData<List<Movie>>
 }
