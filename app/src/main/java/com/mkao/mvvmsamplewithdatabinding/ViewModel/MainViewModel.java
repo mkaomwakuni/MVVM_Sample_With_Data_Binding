24bbdf77a0b114947e29d3f4e9b40337afd75d66
mkao <mkaoibrahim@gmail.com>
2022-05-24 21:36:12 +0300

    ViewModels

---
+++ app/src/main/java/com/mkao/mvvmsamplewithdatabinding/ViewModel/MainViewModel.java
@@ -0,0 +1,4 @@
+package com.mkao.mvvmsamplewithdatabinding.ViewModel;
+
+public class MainViewModel {
+}
---
+++ app/src/main/java/com/mkao/mvvmsamplewithdatabinding/ViewModel/SearchViewModel.kt
@@ -0,0 +1,4 @@
+package com.mkao.mvvmsamplewithdatabinding.ViewModel
+
+class SearchViewModel {
+}
