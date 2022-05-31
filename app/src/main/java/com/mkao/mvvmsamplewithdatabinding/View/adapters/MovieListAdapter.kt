03919d00565cf3e2192b08f06176558417180a97
mkao <mkaoibrahim@gmail.com>
2022-05-31 07:04:29 -1200

    adapters

--- app/src/main/java/com/mkao/mvvmsamplewithdatabinding/View/adapters/MovieListAdapter.kt
+++ app/src/main/java/com/mkao/mvvmsamplewithdatabinding/View/adapters/MovieListAdapter.kt
@@ -1,55 +1,28 @@
 package com.mkao.mvvmsamplewithdatabinding.View.adapters
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.mkao.mvvmsamplewithdatabinding.R
 import com.mkao.mvvmsamplewithdatabinding.data.model.Movie
-import com.squareup.picasso.Picasso
-import retrofit2.Retrofit
 
-class MovieListAdapter(private val moviesList: MutableList<Movie>):RecyclerView.Adapter<MovieListAdapter.MovieHolder>() {
+class MovieListAdapter (private val movies:MutableList<Movie>):RecyclerView.Adapter<MovieListAdapter.MovieHolder>() {
     val selectedMovies = HashSet<Movie>()
+
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): MovieListAdapter.MovieHolder {
-        val  view = LayoutInflater.from(parent.context)
+        val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_movie_main,parent,false)
-        return MovieHolder(view)
+        return SearchListAdapter.MovieHolder(view)
     }
-    fun setMovies (moviesList: MutableList<Movie>){
-        this.movies.clear()
-        this.movies.addALL(moviesList)
-        notifyDataSetChanged()
-    }
-    override fun onBindViewHolder(holder: MovieHolder, position: Int) {
+
+    override fun onBindViewHolder(holder: MovieListAdapter.MovieHolder, position: Int) {
         holder.bind(movies[position])
     }
-
-    override fun getItemCount(): Int  = moviesList.size
-    //binding the ui
-    inner class MovieHolder(val view: View): RecyclerView.ViewHolder(view) {
-        fun bind(movie: Movie) = with(view){
-            movieTitleTextView.Text = movie.title
-            movieReleaseDateTextView.text = movie.releaseDate
-            checkBx.isChecked  = movie.watched
-
-            if (movie.posterPath!=null)
-                Picasso.get().load(Retrofit.TMDB_IMAGEURL + movie.posterPath).into(movieImageView)
-            else{
-                movieImageView.setImageDrawable(ResourcesCompat.getDrawable(resources, R.drawable.ic_local_movies_gray,null))
-            }
-            checkBx.setOnCheckedChangeListener{checkBx,isChecked ->
-                if (!selectedMovies.contains(movie) && isChecked){
-                    selectedMovies.add(movie)
-                }
-                else{
-                    selectedMovies.remove(movie)
-                }
-            }
-        }
+    fun setMovies(movieList: List<Movie>){
+        this.movies.clear()
+        this.movies.addAll(movieList)
     }
 }
--- app/src/main/java/com/mkao/mvvmsamplewithdatabinding/View/adapters/SearchListAdapter.kt
+++ app/src/main/java/com/mkao/mvvmsamplewithdatabinding/View/adapters/SearchListAdapter.kt
@@ -1,4 +1,55 @@
 package com.mkao.mvvmsamplewithdatabinding.View.adapters
 
-class SearchListAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.mkao.mvvmsamplewithdatabinding.R
+import com.mkao.mvvmsamplewithdatabinding.data.model.Movie
+import com.squareup.picasso.Picasso
+import retrofit2.Retrofit
+
+class SearchListAdapter(private val moviesList: MutableList<Movie>):RecyclerView.Adapter<SearchListAdapter.MovieHolder>() {
+    val selectedMovies = HashSet<Movie>()
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): SearchListAdapter.MovieHolder {
+        val  view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_movie_main,parent,false)
+        return MovieHolder(view)
+    }
+    fun setMovies (moviesList: MutableList<Movie>){
+        this.movies.clear()
+        this.movies.addALL(moviesList)
+        notifyDataSetChanged()
+    }
+    override fun onBindViewHolder(holder: MovieHolder, position: Int) {
+        holder.bind(movies[position])
+    }
+
+    override fun getItemCount(): Int  = moviesList.size
+    //binding the ui
+    inner class MovieHolder(val view: View): RecyclerView.ViewHolder(view) {
+        fun bind(movie: Movie) = with(view){
+            movieTitleTextView.Text = movie.title
+            movieReleaseDateTextView.text = movie.releaseDate
+            checkBx.isChecked  = movie.watched
+
+            if (movie.posterPath!=null)
+                Picasso.get().load(Retrofit.TMDB_IMAGEURL + movie.posterPath).into(movieImageView)
+            else{
+                movieImageView.setImageDrawable(ResourcesCompat.getDrawable(resources, R.drawable.ic_local_movies_gray,null))
+            }
+            checkBx.setOnCheckedChangeListener{checkBx,isChecked ->
+                if (!selectedMovies.contains(movie) && isChecked){
+                    selectedMovies.add(movie)
+                }
+                else{
+                    selectedMovies.remove(movie)
+                }
+            }
+        }
+    }
 }
